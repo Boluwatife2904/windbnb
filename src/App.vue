4ebdc4ce6100e6d85e2992d7d8e38c2299711cd0
unknown <sanvicola2000@gmail.com>
2021-05-01 18:43:54 +0100

    Added empty screen to indicate no result found

--- src/App.vue
+++ src/App.vue
@@ -32,12 +32,18 @@
           :key="apartment"
         ></apartment-loader>
       </div>
-      <div class="all-apartments" v-else>
+      <div class="all-apartments" v-else-if="!isLoading && filteredApartments && filteredApartments.length > 0">
         <single-apartment
           v-for="apartment in filteredApartments"
           :key="apartment.title"
           :apartment="apartment"
         ></single-apartment>
+      </div>
+      <div class="no-apartment-found" v-else>
+        <img src="@/assets/error.png" alt="Error Image">
+        <h6>No Result Found</h6>
+        <p>We've searched more than 350 hotels and did not find any hotels for your search</p>
+        <button @click="clearSearch">Search Again</button>
       </div>
     </div>
     <teleport to="#app">
@@ -326,6 +332,12 @@ export default {
       }
       return apartments;
     },
+    clearSearch() {
+      this.location = null;
+      this.guests = null;
+      this.numberOfAdults = 0;
+      this.numberOfChildren = 0;
+    },
     clearLocation() {
       this.location = null;
     },
@@ -492,6 +504,46 @@ export default {
       align-items: center;
       flex-wrap: wrap;
       width: 100%;
+    }
+  }
+
+  .no-apartment-found {
+    max-width: 400px;
+    width: 90%;
+    margin: 10px auto 20px;
+    text-align: center;
+
+    img {
+      height: 200px;
+      display: block;
+      margin: 0 auto 20px;
+    }
+
+    h6 {
+      font-weight: 600;
+      font-size: 20px;
+      margin-bottom: 14px;
+    }
+
+    p {
+      color : #222;
+      margin-bottom: 14px;
+      line-height: 30px;
+    }
+
+    button {
+      border: none;
+      background: #eb5757;
+      color: #fff;
+      font: inherit;
+      border-radius: 6px;
+      padding: 14px 30px;
+      cursor: pointer;
+      font-weight: 500;
+
+      :focus {
+        outline: none;
+      }
     }
   }
 }
