09f775dcd542b92e22cc1e6009ea604fe86f3131
unknown <sanvicola2000@gmail.com>
2021-05-02 20:27:03 +0100

    Started dividing app into components to allow for modularity

---
+++ src/components/EmptySearch.vue
@@ -0,0 +1,62 @@
+<template>
+  <!-- EMPTY SCREEN WHEN NO APARTMENT IS FOUND -->
+  <div class="no-apartment-found">
+    <img src="@/assets/error.png" alt="Error Image" />
+    <h6>No Result Found</h6>
+    <p>
+      We've searched more than 350 hotels and did not find any hotels for your
+      search
+    </p>
+    <button @click="$emit('clear-search')">Search Again</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "EmptySearch",
+  emits: ["clear-search"]
+};
+</script>
+
+<style lang="scss">
+//  EMPTY SCREEN
+.no-apartment-found {
+  max-width: 400px;
+  width: 90%;
+  margin: 10px auto 20px;
+  text-align: center;
+
+  img {
+    height: 200px;
+    display: block;
+    margin: 0 auto 20px;
+  }
+
+  h6 {
+    font-weight: 600;
+    font-size: 20px;
+    margin-bottom: 14px;
+  }
+
+  p {
+    color: #222;
+    margin-bottom: 14px;
+    line-height: 30px;
+  }
+
+  button {
+    border: none;
+    background: #eb5757;
+    color: #fff;
+    font: inherit;
+    border-radius: 6px;
+    padding: 14px 30px;
+    cursor: pointer;
+    font-weight: 500;
+
+    :focus {
+      outline: none;
+    }
+  }
+}
+</style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,50 @@
+<template>
+  <!-- FOOTER -->
+  <footer>
+    <p>
+      Created by
+      <span class="username"
+        ><a href="https://twitter.com/developerBolu">Boluwatife</a></span
+      >
+      -
+      <span class="devchallenges"
+        ><a href="https://devchallenges.io">devChallenges.io</a></span
+      >
+    </p>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+};
+</script>
+
+<style lang="scss">
+// FOOTER
+footer {
+  text-align: center;
+  margin-top: 30px;
+  margin-bottom: 20px;
+
+  p {
+    margin: 0;
+    font-weight: 500;
+    font-size: 14px;
+    line-height: 17px;
+    color: #828282;
+
+    .username {
+      font-weight: 700;
+    }
+
+    a {
+      color: inherit;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+}
+</style>
